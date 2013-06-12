4c646b7f9a58960f65aefb36de6f8f4999d0628c
Chris Briesemeister <breeze@breeze-ubuntu-1204.(none)>
2013-06-11 22:50:02 -0500

    Breeze: adding drupal data load

--- puppet/modules/carp_content/manifests/init.pp
+++ puppet/modules/carp_content/manifests/init.pp
@@ -1,7 +1,13 @@
-class carp_content {
+class carp_content inherits mysql {
 
-  exec {"echo hello":
-    command => "//bin/echo hi | /bin/cat >> /tmp/hi.txt",
-    require => Class[ "mysql" ]
+  exec {"import data":
+    command => "/bin/gunzip < /tmp/carp_website_backup-may-21-2013.sql.gz | /usr/bin/mysql -u root -p${mysqlPassword} drupal6",
+    require => [Class[ "mysql" ], File[ "/tmp/carp_website_backup-may-21-2013.sql.gz" ]]
+  }
+  file { "/tmp/carp_website_backup-may-21-2013.sql.gz":
+    ensure  => present,
+    mode    => 644,
+    source  => "puppet:///modules/carp_content/tmp/carp_website_backup-may-21-2013.sql.gz",
+
   }
 }
