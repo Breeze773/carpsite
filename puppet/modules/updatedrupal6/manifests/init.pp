2e64882d937a6a7647c4ef9ff643bcc32715d1ea
Chris Briesemeister <breeze@breeze-ubuntu-1204.(none)>
2013-07-02 18:04:44 -0500

    new version of captcha module and making sure script exists before trying to exec it

--- puppet/modules/updatedrupal6/manifests/init.pp
+++ puppet/modules/updatedrupal6/manifests/init.pp
@@ -4,10 +4,15 @@ include apache2
     ensure  => directory,
     source  => "puppet:///modules/updatedrupal6/tmp/mod-updates",
     recurse => true,
-  }            
-  exec { "upgrade modules":
+  }
+  file { "/tmp/mod-updates/module-update.sh":
+    ensure  => present,
+    source  => "puppet:///modules/updatedrupal6/tmp/mod-updates/module-update.sh",
+  }
+
+exec { "upgrade modules":
     command  => "/tmp/mod-updates/module-update.sh",
-    require  => File[ "/tmp/mod-updates/" ],
+    require  => File[ "/tmp/mod-updates/module-update.sh" ],
     notify   => Service[ "apache2" ]
   }
  }
